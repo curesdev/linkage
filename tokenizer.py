0d07f24c9d3c1a5e17c71c4be94f54aac1b4501e
Víctor López Ferrando <victor@iomob.net>
2023-05-19 21:47:57 +0200

    Add notebook that runs both tokenization + linkage

--- tokenizer.py
+++ tokenizer.py
@@ -10,7 +10,7 @@ def tokenize(l, k, eps, fields):
         for i in range(k):
             bf[int(sha256(f"{field}#{i}".encode("utf-8")).hexdigest(), 16) % l] = 1
     eta = 1.0 - 1.0 / (1.0 + exp(eps))
-    return [bit if np.random.random() <= eta else 1 - bit for bit in bf]
+    return np.array([bit if np.random.random() <= eta else 1 - bit for bit in bf], dtype=np.uint8)
 
 
 def q_grams(s, q=2, prefix=""):
@@ -22,6 +22,25 @@ def q_grams(s, q=2, prefix=""):
 
 def expand_single_value(s):
     return [f"{s}:{x}" for x in range(1, 6)]
+
+
+def tokenize_name(l, kn, eps, name, prefix):
+    expanded = q_grams(name, prefix=prefix)
+    k = 1 + kn // len(set(expanded))
+    return tokenize(l, k, eps, expanded)
+
+
+def digit_q_grams(s, q=2, prefix=""):
+    s = "".join(filter(str.isdigit, s.lower()))
+    if len(s) < q:
+        return [s]
+    return [prefix + s[i : i + q] for i in range(len(s) - q + 1)]
+
+
+def tokenize_number(l, kn, eps, name, prefix):
+    expanded = digit_q_grams(name, prefix=prefix)
+    k = 1 + kn // len(set(expanded))
+    return tokenize(l, k, eps, expanded)
 
 
 def pii_tokenize(l, k, eps, first_name, last_name, city, date_of_birth, gender):
@@ -38,6 +57,4 @@ def pii_tokenize(l, k, eps, first_name, last_name, city, date_of_birth, gender):
 
 
 def dice(a, b) -> float:
-    a = np.array(a)
-    b = np.array(b)
     return 2 * (a & b).sum() / (a.sum() + b.sum())
