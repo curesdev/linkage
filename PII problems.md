3ba783f37c1f12a97c8735967e3472cdbb96325d
Víctor López Ferrando <victor@iomob.net>
2023-07-20 07:37:44 +0200

    Work in progress

---
+++ PII problems.md
@@ -0,0 +1,31 @@
+
+
+
+# List of problems with the PII
+
+## Name values
+
+1. 1 letter edits: delete, add, replace
+2. 2 consecutive letter edits: delete, add, replace
+3. 2 non-consecutive letter edits: delete, add, replace
+4. 3 consecutive letter edits: delete, add, replace
+5. 3 non-consecutive letter edits: delete, add, replace
+6. 4 consecutive letter edits: delete, add, replace
+7. 4 non-consecutive letter edits: delete, add, replace
+8. Only first letter taken
+9. Common phonetic misspellings
+
+10. Missing middle name
+11. Missing last name
+12. Middle name and last name swapped
+13. First name and last name swapped
+
+## Date of birth
+
+1. 1 day before
+2. 1 day after
+3. First day of the same month
+4. First day of the same year
+5. 2 days before
+6. 2 days after
+
--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,34 @@
+from collections import defaultdict
 from hashlib import sha256
 from math import exp
 
 import numpy as np
 
+
+
+@dataclass
+class PIITokenizer:
+    first_name: str
+    middle_name: str
+    last_name: str
+    full_name: str
+    first_name_soundex: str
+    last_name_soundex: str
+    gender: str
+    date_of_birth: str
+    city_at_birth: str
+    address_at_bith: str
+    state_at_birth: str
+    country_at_birth: str
+
+
+
+
+
+
+
+
+# Tokenize
 
 def tokenize(l, k, eps, fields):
     bf = [0] * l
@@ -13,11 +39,61 @@ def tokenize(l, k, eps, fields):
     return np.array([bit if np.random.random() <= eta else 1 - bit for bit in bf], dtype=np.uint8)
 
 
+# Normalize name
+
+
+def normalize_name(name):
+    return "".join(filter(str.isalpha, name.lower()))
+
+
+# Q-grams
+
 def q_grams(s, q=2, prefix=""):
-    s = "".join(filter(str.isalpha, s.lower()))
+    s = normalize_name(s)
     if len(s) < q:
-        return [s]
-    return [prefix + s[i : i + q] for i in range(len(s) - q + 1)]
+        return [s] + list(s)
+
+    times_seen = defaultdict(int)
+    grams = []
+    for i in range(len(s) - q + 1):
+        gram = s[i : i + q]
+        times_seen[gram] += 1
+        grams += [f"{prefix}{gram}:{times_seen[gram]}"]
+
+    return grams
+
+
+def soundex(token):
+    """Source: https://www.geeksforgeeks.org/implement-phonetic-search-in-python-with-soundex-algorithm/"""
+    token = token.upper()
+    soundex = ""
+ 
+    # Retain the First Letter
+    soundex += token[0]
+ 
+    # Create a dictionary which maps
+    # letters to respective soundex
+    # codes. Vowels and 'H', 'W' and
+    # 'Y' will be represented by '.'
+    dictionary = {"BFPV": "1", "CGJKQSXZ": "2",
+                  "DT": "3",
+                  "L": "4", "MN": "5", "R": "6",
+                  "AEIOUHWY": "."}
+ 
+    # Enode as per the dictionary
+    for char in token[1:]:
+        for key in dictionary.keys():
+            if char in key:
+                code = dictionary[key]
+                if code != '.':
+                    if code != soundex[-1]:
+                        soundex += code
+ 
+    # Trim or Pad to make Soundex a
+    # 4-character code
+    soundex = soundex[:7].ljust(7, "0")
+ 
+    return soundex
 
 
 def expand_single_value(s):
@@ -58,3 +134,8 @@ def pii_tokenize(l, k, eps, first_name, last_name, city, date_of_birth, gender):
 
 def dice(a, b) -> float:
     return 2 * (a & b).sum() / (a.sum() + b.sum())
+
+
+def similarity(a, b) -> float:
+    assert len(a) == len(b)
+    return (a == b).sum() / len(a)
